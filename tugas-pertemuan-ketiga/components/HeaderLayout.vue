0fdf51ee225a7e130738f9487c871deb33416e49
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-09-10 22:23:34 +0700

    feat: :sparkles: add task 3

---
+++ tugas-pertemuan-ketiga/components/HeaderLayout.vue
@@ -0,0 +1,234 @@
+<template>
+  <header class="navbar px-4 fixed w-full">
+    <div class="container mx-auto flex items-center justify-between py-4">
+      <div class="">
+        <nuxt-link to="/">
+          <h1 class="text-center md:text-start text-3xl font-bold text-white">
+            Rent<span class="text-orange-400">Here</span>
+          </h1>
+        </nuxt-link>
+      </div>
+      <nav class="hidden lg:flex space-x-4">
+        <nuxt-link :to="linkHome" class="nav-link">Home</nuxt-link>
+        <nuxt-link :to="linkCars" class="nav-link">Cars</nuxt-link>
+        <nuxt-link :to="linkBikes" class="nav-link">Bikes</nuxt-link>
+        <nuxt-link :to="linkServices" class="nav-link">Services</nuxt-link>
+        <nuxt-link :to="linkTestimonials" class="nav-link"
+          >Testimonials</nuxt-link
+        >
+        <nuxt-link :to="linkContact" class="nav-link">Contact</nuxt-link>
+      </nav>
+      <nuxt-link to="/login">
+        <button type="button" class="hidden lg:block login-button">
+          Login
+        </button>
+      </nuxt-link>
+      <button
+        type="button"
+        class="flex lg:hidden hamburger"
+        title="Menu"
+        @click="toggleSidebar"
+      >
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </button>
+    </div>
+    <div v-if="showSidebar" class="sidebar" :class="sidebarClasses">
+      <button type="button" class="sidebar-close" @click="toggleSidebar">
+        X
+      </button>
+      <nav class="sidebar-links">
+        <nuxt-link :to="linkHome" class="sidebar-link">Home</nuxt-link>
+        <nuxt-link :to="linkCars" class="sidebar-link">Cars</nuxt-link>
+        <nuxt-link :to="linkBikes" class="sidebar-link">Bikes</nuxt-link>
+        <nuxt-link :to="linkServices" class="sidebar-link">Services</nuxt-link>
+        <nuxt-link :to="linkTestimonials" class="sidebar-link"
+          >Testimonials</nuxt-link
+        >
+        <nuxt-link :to="linkContact" class="sidebar-link">Contact</nuxt-link>
+        <nuxt-link :to="linkLogin">
+          <button type="button" class="sidebar-login-button">Login</button>
+        </nuxt-link>
+      </nav>
+    </div>
+    <div v-if="showOverlay" class="overlay" @click="toggleSidebar"></div>
+  </header>
+</template>
+
+<script>
+import {
+  linkHome,
+  linkContact,
+  linkCars,
+  linkBikes,
+  linkServices,
+  linkTestimonials,
+  linkLogin,
+} from '@/helpers/linkData'
+export default {
+  data() {
+    return {
+      showSidebar: false,
+      showOverlay: false,
+      linkHome,
+      linkContact,
+      linkCars,
+      linkBikes,
+      linkServices,
+      linkTestimonials,
+      linkLogin,
+    }
+  },
+  computed: {
+    sidebarClasses() {
+      return this.showSidebar ? 'active' : 'deactive'
+    },
+  },
+  methods: {
+    toggleSidebar() {
+      this.showSidebar = !this.showSidebar
+      this.showOverlay = !this.showOverlay
+    },
+  },
+}
+</script>
+
+<style scoped>
+.nav-link.nuxt-link-exact-active {
+  @apply text-orange-400 border-b border-b-orange-400;
+}
+/* Navbar styles */
+.navbar {
+  background-color: #333;
+  color: white;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: white;
+  @apply duration-300 transition-colors;
+}
+
+.nav-link:hover,
+.sidebar-link:hover {
+  @apply hover:text-orange-400;
+}
+
+.login-button {
+  background-color: transparent;
+  border: 1px solid white;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  @apply duration-300 transition-colors;
+}
+
+.login-button:hover,
+.sidebar-login-button:hover {
+  @apply hover:text-orange-400 hover:border border-orange-400;
+}
+
+.hamburger {
+  background-color: transparent;
+  border: none;
+  color: white;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 24px;
+  width: 24px;
+}
+
+.bar {
+  background-color: white;
+  display: flex;
+  gap: 5px;
+  height: 4px;
+  width: 100%;
+}
+
+/* Sidebar styles */
+.sidebar {
+  position: fixed;
+  top: 0;
+  width: 250px;
+  height: 100%;
+  background-color: #333;
+  color: white;
+  z-index: 10;
+}
+
+.sidebar.active {
+  animation: slideIn 0.5s ease-in-out forwards;
+}
+
+.sidebar.deactive {
+  animation: slideOut 0.5s ease-in-out forwards;
+}
+
+@keyframes slideIn {
+  from {
+    left: -100%;
+  }
+  to {
+    left: 0;
+  }
+}
+
+@keyframes slideOut {
+  from {
+    left: 0;
+  }
+  to {
+    left: -100%;
+  }
+}
+
+.sidebar-close {
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 24px;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  cursor: pointer;
+}
+
+.sidebar-links {
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar-link {
+  text-decoration: none;
+  color: white;
+  margin-bottom: 16px;
+  @apply duration-300 transition-colors;
+}
+
+.sidebar-login-button {
+  background-color: transparent;
+  border: 1px solid white;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 100%;
+  @apply duration-300 transition-colors;
+}
+
+/* Overlay styles */
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+}
+</style>
---
+++ tugas-pertemuan-ketiga/helpers/carsDataDummy.js
@@ -0,0 +1,145 @@
+const carsData = [
+  {
+    id: 1,
+    name: 'Honda CR-V Hybrid',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2022/07/13102850/Honda-CR-V-generasi-terbaru_0.jpg',
+    category: 'Honda',
+    description: 'The Honda CR-V Hybrid is a fuel-efficient hybrid SUV.',
+    qty: 5,
+    price: 35000,
+  },
+  {
+    id: 2,
+    name: 'Mitsubishi XFC / Mitsubishi Xforce',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2022/10/20100514/mitsubishi-xfc-test-drive.jpg',
+    category: 'Mitsubishi',
+    description: 'The Mitsubishi XFC is a stylish and modern crossover SUV.',
+    qty: 8,
+    price: 30000,
+  },
+  {
+    id: 3,
+    name: 'KIA EV9',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/05/26202257/2024-kia-ev9.jpg',
+    category: 'KIA',
+    description: 'The KIA EV9 is an all-electric SUV with advanced technology.',
+    qty: 3,
+    price: 50000,
+  },
+  {
+    id: 4,
+    name: 'Maxus MIFA 9',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/01/17115354/mpv-mg-mifa-9.jpg',
+    category: 'Maxus',
+    description: 'The Maxus MIFA 9 is a spacious and versatile MPV.',
+    qty: 6,
+    price: 40000,
+  },
+  {
+    id: 5,
+    name: 'Citroen C3 Aircross',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/04/29174525/citroen-c3-aircross-tampak-depan.jpg',
+    category: 'Citroen',
+    description: 'The Citroen C3 Aircross is a compact and stylish SUV.',
+    qty: 10,
+    price: 25000,
+  },
+  {
+    id: 6,
+    name: 'SUV Daihatsu D66B',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2022/12/06145035/Perodua-Nexis-Photo.jpg',
+    category: 'Daihatsu',
+    description: 'The SUV Daihatsu D66B offers comfort and performance.',
+    qty: 7,
+    price: 30000,
+  },
+  {
+    id: 7,
+    name: 'Hyundai Ioniq 6',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2022/12/29175609/hyundai-ioniq-6-semakin-dekat-indonesia.jpg',
+    category: 'Hyundai',
+    description: 'The Hyundai Ioniq 6 is a futuristic electric sedan.',
+    qty: 4,
+    price: 40000,
+  },
+  {
+    id: 8,
+    name: 'Hyundai Stargazer X',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/06/23094805/pemesanan-stargazer-cross.jpg',
+    category: 'Hyundai',
+    description: 'The Hyundai Stargazer X offers a unique design.',
+    qty: 2,
+    price: 35000,
+  },
+  {
+    id: 9,
+    name: 'Chery Tiggo 4 Pro',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/06/15104408/chery-tiggo-4-pro-comfort-vs-premium.jpg',
+    category: 'Chery',
+    description: 'The Chery Tiggo 4 Pro offers comfort and style.',
+    qty: 6,
+    price: 30000,
+  },
+  {
+    id: 10,
+    name: 'Suzuki Jimny 5 Pintu',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/01/13155104/eksterior-suzuki-jimny-5-pintu.jpg',
+    category: 'Suzuki',
+    description:
+      'The Suzuki Jimny 5 Pintu is a compact SUV with off-road capability.',
+    qty: 9,
+    price: 25000,
+  },
+  {
+    id: 11,
+    name: 'Nissan X-Trail e-Power / X-Trail T33',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2022/07/21013814/Nissan-X-Trail-G-e-4ORCE.jpg',
+    category: 'Nissan',
+    description: 'The Nissan X-Trail e-Power is an efficient hybrid SUV.',
+    qty: 5,
+    price: 35000,
+  },
+  {
+    id: 12,
+    name: 'Suzuki XL7 Hybrid',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/06/15161224/image-3-4.png',
+    category: 'Suzuki',
+    description: 'The Suzuki XL7 Hybrid is a family-friendly SUV.',
+    qty: 7,
+    price: 40000,
+  },
+  {
+    id: 13,
+    name: 'Toyota Yaris Cross',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2023/05/13145626/harga-yarris-cross-2023-tampak-depan-1.jpg',
+    category: 'Toyota',
+    description: 'The Toyota Yaris Cross is a compact crossover SUV.',
+    qty: 4,
+    price: 30000,
+  },
+  {
+    id: 14,
+    name: 'Mazda CX-60 Hybrid',
+    image:
+      'https://s3.ap-southeast-1.amazonaws.com/moladin.assets/blog/wp-content/uploads/2022/11/28150829/mazda-cx-60-2022.jpg',
+    category: 'Mazda',
+    description: 'The Mazda CX-60 Hybrid offers a premium driving experience.',
+    qty: 3,
+    price: 35000,
+  },
+]
+
+export default carsData
